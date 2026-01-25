2f7e17333205466b8b7d7bde8b1b53a92bc14f09
PND <fujjisaaan@gmail.com>
2026-01-25 23:00:29 +0900

    chore: release v0.3.5

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -12,7 +12,7 @@ import { synthCommand } from "./commands/synth.js";
 const parsed = cli(
   {
     name: "tfts",
-    version: "0.3.4",
+    version: "0.3.5",
     help: {
       description: "Terraform TypeScript SDK",
     },
